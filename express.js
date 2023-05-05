223d97c5bdabd6668b50cce90d6d4ffaabca0fec
zdosijd <1162528933@qq.com>
2023-05-05 14:58:05 +0800

    ceshi

---
+++ express.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()//建立应用对象
+
+app.get('/ie',(request,a)=>{
+    a.setHeader('Access-Control-Allow-Origin', '*')
+    setTimeout(() => {
+    a.send('hello，我是响应')
+    }, 2000);
+})
+app.listen('8000',()=>{
+    console.log('8000端口启动');
+})
